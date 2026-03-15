15e6b2ad49b10b55876b3466795f5f5e8174fd89
Maanas Kaniti <150051799+Maanas-kaniti@users.noreply.github.com>
2026-03-15 11:06:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Array Subset/array-subset.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    
+    public boolean isSubset(int a[], int b[]) {
+        // Your code here
+        HashMap<Integer,Integer> map = new HashMap<>();
+        if(b.length>a.length){
+            return false;
+        }
+        for(int num : a){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        
+        for(int num : b){
+            if(!map.containsKey(num) || map.get(num)==0){
+                return false;
+            }
+            map.put(num,map.get(num)-1);
+        }
+        
+        return true;
+    }
+    
+}
