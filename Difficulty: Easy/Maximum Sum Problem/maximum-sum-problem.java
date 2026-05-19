bf45bae9b8050246e4143aa0fd08be104063c160
Maanas Kaniti <150051799+Maanas-kaniti@users.noreply.github.com>
2026-05-19 15:47:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Maximum Sum Problem/maximum-sum-problem.java
+++ Difficulty: Easy/Maximum Sum Problem/maximum-sum-problem.java
@@ -1,11 +1,19 @@
 class Solution {
-    public int maxSum(int n) {
+    static int solve(int n, int[] dp){
         int n1 = n/2;
         int n2 = n/3;
         int n3 = n/4;
         if(n1+n2+n3<=n){
             return n;
         }
-        return maxSum(n/2)+maxSum(n/3)+maxSum(n/4);
+        if(dp[n]!=0){
+            return dp[n];
+        }
+        dp[n] = solve(n/2,dp)+solve(n/3,dp)+solve(n/4,dp);
+        return dp[n];
+    }
+    public int maxSum(int n) {
+        int[] dp = new int[n+1];
+        return solve(n , dp);
     }
 }
