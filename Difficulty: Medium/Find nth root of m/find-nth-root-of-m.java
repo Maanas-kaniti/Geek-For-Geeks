a4f15ab2275900a12fb1c8203d5c2ff50e5d8b11
Maanas Kaniti <150051799+Maanas-kaniti@users.noreply.github.com>
2026-06-12 11:40:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Find nth root of m/find-nth-root-of-m.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int nthRoot(int n, int m) {
+        // code here
+        int l = 0;
+        int r = m;
+        while(l<=r){
+            int mid = l+(r-l)/n;
+            if((long)Math.pow(mid,n)==m){
+                return mid;
+            }
+            else if((long)Math.pow(mid,n)<m){
+                l = mid+1;
+            }
+            else r = mid-1;
+        }
+        return -1;
+    }
+}
